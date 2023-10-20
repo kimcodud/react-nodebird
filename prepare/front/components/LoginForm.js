997d2c30948639fa875c95f22ac4c14753962feb
김채영 <cccodud@gmail.com>
2023-10-20 13:16:37 +0900

    Feat: 회원가입 페이지 관련 custom hook 생성

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useMemo } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
+import propTypes from 'prop-types';
 
 const ButtonWrapper = styled.div`
   margintop: 10px;
@@ -12,12 +13,11 @@ const FormWapper = styled(Form)`
 `;
 const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-
   const onChangeId = useCallback((e) => {
     setId(e.target.value);
   }, []);
 
+  const [password, setPassword] = useState('');
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
   }, []);
@@ -57,6 +57,10 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </ButtonWrapper>
     </FormWapper>
   );
+};
+
+LoginForm.propTypes = {
+  setIsLoggedIn: propTypes.func.isRequired,
 };
 
 export default LoginForm;
--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import AppLayout from '../components/AppLayout';
+import React, { useCallback, useState } from 'react';
 import Head from 'next/head';
+import { Form, Input } from 'antd';
+import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
+  const [id, setId] = useState('');
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const [nickname, setNickname] = useState('');
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const [password, setPassword] = useState('');
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {}, []);
+
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>회원가입 | NodeBird</title>
       </Head>
-      <AppLayout>
-        <div>회원가입 페이지</div>
-      </AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nickname">닉네임</label>
+          <br />
+          <Input
+            name="user-nickname"
+            value={nickname}
+            required
+            onChange={onChangeNickname}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">비밀번호 체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
