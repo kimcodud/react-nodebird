6398ea6220f0311f807a6d0f10104b0cd47e6fbd
김채영 <cccodud@gmail.com>
2023-11-10 15:56:55 +0900

    Feat: take, delay 효과 추가

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,4 +1,4 @@
-import { all, fork, call, take, put } from 'redux-saga/effects';
+import { all, fork, call, takeLatest, put, delay } from 'redux-saga/effects';
 import axios from 'axios';
 
 //*붙이지 말것, 실제 서버 요청
@@ -8,12 +8,14 @@ function logInAPI(data) {
 
 function* logIn(action) {
   try {
+    //서버 구현 전까지 비동기 효과 주기
+    yield delay(1000);
+    // const result = yield call(logInAPI);
     //fork -> 함수 실행 =/= call (둘 다 generator 함수 실행)
     //fork는 비동기 함수 호출(non-blocking),  call은 동기 함수 호출(blocking)
-    const result = yield call(logInAPI, action.data);
     yield put({
       type: 'LOG_IN_SUCCESS',
-      data: result.data,
+      // data: result.data,
     });
   } catch (err) {
     yield put({
@@ -29,10 +31,11 @@ function logOutAPI() {
 
 function* logOut() {
   try {
-    const result = yield call(logOutAPI);
+    yield delay(1000);
+    // const result = yield call(logOutAPI);
     yield put({
       type: 'LOG_OUT_SUCCESS',
-      data: result.data,
+      // data: result.data,
     });
   } catch (err) {
     yield put({
@@ -46,12 +49,13 @@ function addPostAPI() {
   return axios.post('/api/post');
 }
 
-function* logOut() {
+function* addPost() {
   try {
-    const result = yield call(addPostAPI);
+    yield delay(1000);
+    // const result = yield call(addPostAPI);
     yield put({
       type: 'ADD_POST_SUCCESS',
-      data: result.data,
+      // data: result.data,
     });
   } catch (err) {
     yield put({
@@ -62,15 +66,20 @@ function* logOut() {
 }
 
 function* watchLogin() {
-  yield take('LOG_IN_REQUEST', logIn);
+  //take는 일회성, 한번 실행 후 이벤트리스너 사라짐 (따라서 while(true)문으로 감싸주면 에러없이 무한 사용 가능)
+  //하지만 반복문은 직관적x -> takeEvery로 대체 가능
+  //실수로 클릭 여러 번 한다면? -> takeLatest 사용시 마지막 요청만 실행
+  yield takeLatest('LOG_IN_REQUEST', logIn);
 }
 
 function* watchLogOut() {
-  yield take('LOG_OUT_REQUEST', logOut);
+  yield takeLatest('LOG_OUT_REQUEST', logOut);
 }
 
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST', addPostAPI);
+  //2초 동안 1번의 요청만 가능
+  // yield throttle('ADD_POST_REQUEST', addPost, 2000);
+  yield takeLatest('ADD_POST_REQUEST', addPost);
 }
 
 export default function* rootSaga() {
